3ada3574d55a5e1daf68ac4448171c76ffc10572
Marcelo Ortiz <tentativafc@hotmail.com>
2019-04-09 14:04:23 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello world Mama.
+            Hello world Mama and Riri.
           </p>
           <a
             className="App-link"
